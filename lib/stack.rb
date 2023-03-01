b2323d1a00e995437b87f14053f1a7846c48551f
Xica Felix <xicasfelix@gmail.com>
2023-03-01 13:04:43 -0500

    commit tree 14d4e2cc6e41e7262c8fb0b330011198859ae4c9

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,43 @@
 # your code here
+
+class Stack
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+       raise 'Stack Overflow' if full?
+            @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        top = @stack.length - 1
+        @stack[top]
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+       @limit && @stack.size == @limit
+    end
+
+    def search(value)
+       index = @stack.index(value)
+       if index
+            @stack.size - index - 1
+       else
+        -1
+       end
+    end
+end
